6b22d68ea5172fb09cf2fc7d290672f78184afa7
Jonathan Strootman <jstroot@iplantcollaborative.org>
2013-11-20 16:42:15 -0700

    Ongoing

---
+++ src/main/java/com/gwt/skeleton/client/view/model/DataSearch.java
@@ -0,0 +1,13 @@
+package com.gwt.skeleton.client.view.model;
+
+public interface DataSearch {
+
+    Object getCreatedWithin();
+
+    /**
+     * Range values
+     * 
+     * @return
+     */
+    Object getModifiedWithin();
+}
